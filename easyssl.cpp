034d20ba70ff376eb88e75df91d20ac6ae693dc2
Blue Ye <zr139898@163.com>
2015-04-22 15:45:28 +0800

    use static lock

--- easyssl.cpp
+++ easyssl.cpp
@@ -18,43 +18,11 @@ static unsigned long id_function(void) {
     return ((unsigned long)THREAD_ID);
 }
 
-// a data structure to hold the data necessary for the mutex.
-struct CRYPTO_dynlock_value {
-  MUTEX_TYPE mutex;
-};
-
-// create a new mutex, allocate memory, and have any necessary initialization.
-// The newly created and initialized mutex should be returned
-// in a released state from the function.
-static struct CRYPTO_dynlock_value * dyn_create_function(const char * file, int line) {
-    struct CRYPTO_dynlock_value * value;
-
-    value = (struct CRYPTO_dynlock_value *)malloc(sizeof(
-            struct CRYPTO_dynlock_value));
-
-    if (!value) return NULL;
-
-    MUTEX_SETUP(value->mutex);
-    return value;
-}
-
-static void dyn_lock_function(int mode, struct CRYPTO_dynlock_value * l,
-                              const char * file, int line) {
-    if (mode & CRYPTO_LOCK) {
-        MUTEX_LOCK(l->mutex);
-    } else {
-        MUTEX_UNLOCK(l->mutex);
-    }
-}
-
-static void dyn_destroy_function(struct CRYPTO_dynlock_value * l,
-                                 const char * file, int line) {
-    MUTEX_CLEANUP(l->mutex);
-    free(l);
-}
-
+// allocate the memory required to hold the mutexes.
 int THREAD_setup(void) {
     int i;
+
+    //CRYPTO_num_locks() returns the required number of locks
     mutex_buf = (MUTEX_TYPE *)malloc(CRYPTO_num_locks() * sizeof(MUTEX_TYPE));
     if (!mutex_buf) return 0;
 
@@ -63,24 +31,16 @@ int THREAD_setup(void) {
     }
     CRYPTO_set_id_callback(id_function);
     CRYPTO_set_locking_callback(locking_function);
-    // The following three CRYPTO_... functions are the OpenSSL functions
-    // for registering the callbacks we implemented above.
-    CRYPTO_set_dynlock_create_callback(dyn_create_function);
-    CRYPTO_set_dynlock_lock_callback(dyn_lock_function);
-    CRYPTO_set_dynlock_destroy_callback(dyn_destroy_function);
-
     return 1;
 }
 
 int THREAD_cleanup(void) {
     int i;
+
     if (!mutex_buf) return 0;
 
     CRYPTO_set_id_callback(NULL);
     CRYPTO_set_locking_callback(NULL);
-    CRYPTO_set_dynlock_create_callback(NULL);
-    CRYPTO_set_dynlock_lock_callback(NULL);
-    CRYPTO_set_dynlock_destroy_callback(NULL);
     for (i = 0; i < CRYPTO_num_locks(); i++) {
         MUTEX_CLEANUP(mutex_buf[i]);
     }
@@ -88,6 +48,7 @@ int THREAD_cleanup(void) {
     mutex_buf = NULL;
     return 1;
 }
+
 //////////////////////////////////////////////////////////////////////
 // Multithread Support end
 //////////////////////////////////////////////////////////////////////
