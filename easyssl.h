280581e5b820bc29e666226db3872894a052b1c3
Blue Ye <zr139898@163.com>
2015-04-22 15:22:50 +0800

    merge ssl_multithread.h/cpp into easyssl.h/cpp

--- easyssl.h
+++ easyssl.h
@@ -19,15 +19,17 @@
 
 #include <openssl/x509_vfy.h>
 
-#include "ssl_multithread.h"
-using namespace std;
-
+//////////////////////////////////////////////////////////////////////
+// Multithread Support begin
+//////////////////////////////////////////////////////////////////////
+// platform-dependent macros
 #ifndef WIN32
+#include <unistd.h>
 #include <pthread.h>
 #define THREAD_CC *
 #define THREAD_TYPE pthread_t
 #define THREAD_CREATE(tid, entry, arg) pthread_create(&(tid), NULL, \
-                                                      (entry), (arg))
+                                                          (entry), (arg))
 #else
 #include <windows.h>
 #define THREAD_CC __cdecl
@@ -37,9 +39,43 @@ using namespace std;
     } while (0)
 #endif
 
+#if defined(WIN32)
+#define MUTEX_TYPE HANDLE
+#define MUTEX_SETUP(x) (x) = CreateMutex(NULL, FALSE, NULL)
+#define MUTEX_CLEANUP(x) CloseHandle(x)
+#define MUTEX_LOCK(x) WaitForSingleObject((x), INFINITE)
+#define MUTEX_UNLOCK(x) RelieaseMutex(x)
+#define THREAD_ID GetCurrentThreadId()
+#elif defined(_POSIX_THREADS)
+// _POSIX_THREADS is normally defined in unistd.h if pthreads are available on your platform.
+#define MUTEX_TYPE       pthread_mutex_t
+#define MUTEX_SETUP(x)   pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x) pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)    pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)  pthread_mutex_unlock(&(x))
+#define THREAD_ID        pthread_self()
+#else
+#error You must define mutex operations appropriate for your platform!
+#endif
+
+// allocate the memory required to hold the mutexes.
+// we must call call THREAD_setup before our programs starts threads
+// or call OpenSSL functions.
+int THREAD_setup(void);
+// reclaim any memory used for the mutexes.
+int THREAD_cleanup(void);
+//////////////////////////////////////////////////////////////////////
+// Multithread Support end
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 #define handle_error(msg) {fprintf(stderr, "** %s:%i %s\n", __FILE__, __LINE__, msg); \
     ERR_print_errors_fp(stderr); \
     exit(-1);}
+
+using namespace std;
 
 class EasySSL_CTX;
 class EasySSL;
